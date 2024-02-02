96162db9781b61d5ecf21fa1ec449bbb06035c4a
aakash-soni-git <127540841+aakash-soni-git@users.noreply.github.com>
2024-02-02 17:23:13 +0100

    Update usage_example.py

--- usage_example.py
+++ usage_example.py
@@ -1,6 +1,6 @@
 from Bitalinomanager import Bitalino_Manager
 
-addr = "00:21:08:35:14:75"
+addr = "00:21:08:35:14:75" # Replace with your device MAC address
 print("Start Bitalino Data Aquisition...")
 Bitalino_Manager.start_aquisition(channels={'A1':'EDA', 'A2':'ECG'},
     samplingRate=100, 
